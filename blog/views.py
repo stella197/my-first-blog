7c5647d024db6de6d7406be1a454dea49511985c
iamowner <oops1997t@gmail.com>
2019-11-09 11:31:37 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,7 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts= Post.objects.filter(
-        published_date__lte=timezone.now()).order_by('published_date')
+    posts= Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 
